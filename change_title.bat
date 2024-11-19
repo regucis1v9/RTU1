2a97fbe09d05aad75794edfa95d1d14318589ac8
Diablo785 <96918963+Diablo785@users.noreply.github.com>
2024-11-19 10:55:03 +0200

    Update change_title.bat

--- change_title.bat
+++ change_title.bat
@@ -1,3 +1,15 @@
 @echo off
 node title_changer.js
+if errorlevel 1 (
+    echo Failed to change title
+    pause
+    exit /b 1
+)
+
+echo Starting the node server...
+start cmd /k node server.js
+
+echo Starting the application...
+start cmd /k npm start
+
 pause
