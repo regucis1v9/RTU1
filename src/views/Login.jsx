c559e4ad3230e7e268594a76f6149c8d18471a23
Regnars Klavins <regnars.klavins@gmail.com>
2024-11-19 10:43:13 +0200

    password inputs parolei

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,23 +1,20 @@
 import React, { useEffect, useState } from 'react';
-import "../styles/overviewStyles.scss"; // Custom styles
+import "../styles/overviewStyles.scss"; 
 import logo from "../images/logo2.png";
 import { Link } from 'react-router-dom';
-import { Input, Button } from '@mantine/core';
+import { Input, Button, PasswordInput } from '@mantine/core';
 const Login = () => {
-  const [showLogin, setShowLogin] = useState(false); // To control login form visibility
-
+  const [showLogin, setShowLogin] = useState(false); 
   useEffect(() => {
-    // Set timeout to trigger login form after logo animation completes
     const logoAnimationTimeout = setTimeout(() => {
       setShowLogin(true);
-    }, 1000); // 4 seconds for logo animation
+    }, 1000); 
 
     return () => clearTimeout(logoAnimationTimeout);
   }, []);
 
   return (
     <div className='landingContainer'>
-      {/* Logo container that animates from the bottom to the top */}
       <div className={`logoContainer ${showLogin ? 'logoExit' : 'logoEnter'}`}>
         <img className='logo' src={logo} alt="Logo" />
         <div className="text">LIOFILIZĀCIJAS ZINĀTNISKĀ LABORATORIJA</div>
@@ -31,16 +28,16 @@ const Login = () => {
                   placeholder="Lietotājvārds"
                   size='xl'
                   mb={20}
+                  w={300}
                 />
               </Input.Wrapper>
-              <Input.Wrapper>
-                <Input
+                <PasswordInput
                   variant='filled'
                   placeholder="Parole"
                   size='xl'
                   mb={20}
+                  w={300}
                 />
-              </Input.Wrapper>
         <Link to="/landing">
           <Button size='md'>PIESLĒGTIES</Button>
         </Link>
