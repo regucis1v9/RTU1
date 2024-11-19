b5d2620625ac250e9cae4df0f8d2426de99e9235
Diablo785 <96918963+Diablo785@users.noreply.github.com>
2024-11-19 12:47:14 +0200

    Add files via upload

--- change_title.bat
+++ change_title.bat
@@ -1,23 +1,3 @@
 @echo off
-echo Palaiž nosaukuma mainītāju...
-
-:: Palaist nosaukuma mainītāju un sagaidīt, līdz tas pabeidz darbu
-call node title_changer.js
-if errorlevel 1 (
-    echo Neizdevās mainīt nosaukumu
-    pause
-    exit /b 1
-)
-
-:: Startēt serverus tikai tad, ja title_changer.js pabeidzas veiksmīgi
-echo.
-echo Nosaukuma maiņa pabeigta. Startē serverus...
-echo.
-
-echo Startē Node serveri...
-start cmd /k node server.js
-
-echo Startē lietotni...
-start cmd /k npm start
-
+npx electron ./electron.js
 pause
