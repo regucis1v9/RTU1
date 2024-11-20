664fe095366b475d9f1fab50bd2418554ff0761a
Martins Raiens Lacis <139060230+MartinRyannn@users.noreply.github.com>
2024-11-20 10:38:13 +0200

    Update electron.js

--- public/electron.js
+++ public/electron.js
@@ -22,7 +22,7 @@ function createWindow() {
         mainWindow.loadURL('http://localhost:3000');
         mainWindow.webContents.openDevTools();
     } else {
-        mainWindow.loadFile(path.join(__dirname, '../config/config.html'));
+        mainWindow.loadFile(path.join(__dirname, '../config.html'));
     }
 
     // Read and send config when requested
