a49790408077566842c6342fafb95d598e6a9291
Regnars Klavins <regnars.klavins@gmail.com>
2024-11-20 14:06:03 +0200

    Update Login.jsx

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import "../styles/overviewStyles.scss"; 
-import logo from "../images/logo2.png";
+import logo from "./images/Screenshot 2024-11-13 at 22.19.19.png"; // Updated to the dynamically uploaded image path
 import { Link } from 'react-router-dom';
 import { Input, Button, PasswordInput } from '@mantine/core';
+
 const Login = () => {
   const [showLogin, setShowLogin] = useState(false); 
+
   useEffect(() => {
     const logoAnimationTimeout = setTimeout(() => {
       setShowLogin(true);
@@ -15,34 +17,31 @@ const Login = () => {
 
   return (
     <div className='landingContainer'>
-  <div className={`logoContainer ${showLogin ? 'logoExit' : 'logoEnter'}`}>
-    <img className="logo" src={logo} alt="Logo" />
-    <div className="text">New Project Title</div>
-  </div>
-</div>
+      <div className={`logoContainer ${showLogin ? 'logoExit' : 'logoEnter'}`}>
+        <img className="logo" src={logo} alt="Logo" />
+        <div className="text">MAMA123</div> 
+      </div>
 
-      {/* Login form that appears after 4 seconds */}
       <div className={`loginContainer ${showLogin ? 'showLogin' : ''}`}>
-              <Input.Wrapper>
-                <Input
-                  variant='filled'
-                  placeholder="Lietotājvārds"
-                  size='xl'
-                  mb={20}
-                  w={300}
-                />
-              </Input.Wrapper>
-                <PasswordInput
-                  variant='filled'
-                  placeholder="Parole"
-                  size='xl'
-                  mb={20}
-                  w={300}
-                />
+        <Input.Wrapper>
+          <Input
+            variant='filled'
+            placeholder="Lietotājvārds"
+            size='xl'
+            mb={20}
+            w={300}
+          />
+        </Input.Wrapper>
+        <PasswordInput
+          variant='filled'
+          placeholder="Parole"
+          size='xl'
+          mb={20}
+          w={300}
+        />
         <Link to="/landing">
           <Button size='md'>PIESLĒGTIES</Button>
         </Link>
-
       </div>
     </div>
   );
