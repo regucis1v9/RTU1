5923c25c7a92c9a82649d81c588abdd04743984d
Martins Raiens Lacis <139060230+MartinRyannn@users.noreply.github.com>
2025-01-14 14:19:17 +0200

    Update OverviewMain.jsx

--- src/views/OverviewMain.jsx
+++ src/views/OverviewMain.jsx
@@ -1,36 +1,35 @@
 import React, { useEffect, useState } from 'react';
-import {Button, useMantineColorScheme, useComputedColorScheme, ActionIcon} from "@mantine/core";
-import {
-    IconAlertOctagonFilled,
-  IconSun, 
-  IconMoon,
-  IconArrowLeft 
-} from '@tabler/icons-react';
-import { Link } from 'react-router-dom';
-import MainChart from '../components/chart/MainChart';
-import SettingsBox from '../components/chart/SettingsBox';
-import ChartSettings from '../components/chart/ChartSettings';
-import Sidebar from '../components/Sidebar';
-import PauseButton from '../components/PauseButton';
-import styles from '../styles/overviewStyles2.scss';
+import { Button, useMantineColorScheme } from '@mantine/core';
+import { IconSun, IconMoon } from '@tabler/icons-react';
 import Overview from './Overview';
 import Overview2 from './Overview2';
 
-import Terminal from '../components/chart/Terminal';
-import {usePause} from "../context/PauseContext";
-
 export default function OverviewMain() {
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
-  const computedColorScheme = useComputedColorScheme('light', { getInitialValueInEffect: true });
-  const buttonColor = computedColorScheme === 'dark' ? 'white' : 'black';
   const isDark = colorScheme === 'dark';
-
-  
 
   return (
     <div className="o-container">
-        <Overview/>
-        <Overview2/>
+      <div className="theme-toggle-wrapper">
+        <ThemeToggleButton isDark={isDark} toggleColorScheme={toggleColorScheme} />
+      </div>
+      <Overview colorScheme={colorScheme} toggleColorScheme={toggleColorScheme} />
+      <Overview2 colorScheme={colorScheme} toggleColorScheme={toggleColorScheme} />
     </div>
   );
 }
+
+const ThemeToggleButton = ({ isDark, toggleColorScheme }) => (
+  <Button
+    onClick={toggleColorScheme}
+    variant="ghost"
+    size="icon"
+    className="mode2-button"
+  >
+    {isDark ? (
+      <IconSun size={20} stroke={1.2} className="light-button" />
+    ) : (
+      <IconMoon size={20} stroke={1.2} className="dark-button" />
+    )}
+  </Button>
+);
